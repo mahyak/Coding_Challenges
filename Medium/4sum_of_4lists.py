c83ccd7f24314a54ea368842f499a6fbb8c90aa6
Mahya Kardani <m.kardaani@gmail.com>
2022-07-25 09:45:13 -0400

    Create 4sum_of_4lists.py

---
+++ Medium/4sum_of_4lists.py
@@ -0,0 +1,17 @@
+class Solution:
+    def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
+        d = {}
+        count = 0
+        
+        for i in nums1:
+            for j in nums2:
+                if i+j in d:
+                    d[i+j] += 1
+                else:
+                    d[i+j] = 1
+        
+        for i in nums3:
+            for j in nums4:
+                if -i-j in d:
+                    count += d[-i-j]
+        return count
