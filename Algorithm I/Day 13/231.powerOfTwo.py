f8bb1d977b79851388be15d5c591f8e39bc85991
Mahya Kardani <m.kardaani@gmail.com>
2023-02-27 12:14:29 -0500

    power of two

---
+++ Algorithm I/Day 13/231.powerOfTwo.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        print(n)
+        if (n == 1):
+            return True
+
+        if (n%2 != 0 or n == 0):
+            return False
+        
+        return self.isPowerOfTwo(n/2)
